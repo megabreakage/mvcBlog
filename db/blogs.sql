8e66cbdebc445a1ae5d9fe79dd8b8c148fb1373c
megabreakage <martin.njau@live.com>
2018-05-02 11:42:56 +0300

    Adds database for for he project

---
+++ db/blogs.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 02, 2018 at 10:37 AM
+-- Server version: 5.7.22-0ubuntu0.17.10.1
+-- PHP Version: 7.1.15-0ubuntu0.17.10.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blogs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `user_role_id` tinyint(1) NOT NULL,
+  `cellphone` varchar(45) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`user_id`, `username`, `email`, `password`, `user_role_id`, `cellphone`, `date_created`) VALUES
+(1, 'breakage', 'martin.njau@live.com', 'breakage', 1, '0727905402', '2018-04-27 09:02:55'),
+(3, 'martin', 'martin.njau@live.com', 'break', 2, '0727905402', '2018-04-27 09:14:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_role`
+--
+
+CREATE TABLE `tbl_user_role` (
+  `role_id` int(11) NOT NULL,
+  `user_role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user_role`
+--
+
+INSERT INTO `tbl_user_role` (`role_id`, `user_role`) VALUES
+(1, 'Admin'),
+(2, 'Subscriber');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tbl_user_role`
+--
+ALTER TABLE `tbl_user_role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `tbl_user_role`
+--
+ALTER TABLE `tbl_user_role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
