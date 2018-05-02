734df7f8701644a594432b68b3a3ff1283ad388c
megabreakage <martin.njau@live.com>
2018-05-02 09:47:11 +0300

    Creates register and login methods to insert and get user

---
+++ application/models/Queries.php
@@ -0,0 +1,18 @@
+<?php class Queries extends CI_Model{
+
+  public function __construct(){
+    parent::__construct();
+  }
+
+  public function register($data){
+     return $this->db->insert('tbl_users', $data);
+  }
+
+  public function login($data){
+    $query = $this->db->get_where('tbl_users', $data);
+    return $query->row();
+
+  }
+
+
+} ?>
