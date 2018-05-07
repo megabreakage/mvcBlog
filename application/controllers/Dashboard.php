6f1d2b2011482b5ca79736d1c7bb3dd74da26338
megabreakage <martin.njau@live.com>
2018-05-07 16:43:19 +0300

    Adds load views functionality to add_post() and creates publish_post()

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,7 +9,13 @@ class Dashboard extends CI_Controller{
   }
 
   public function add_post(){
-    $this->load->view('dashboard');
+    $this->load->view('templates/header');
+    $this->load->view('add_post');
+    $this->load->view('templates/header');
+  }
+
+  public function publishpost(){
+
   }
 }
 
