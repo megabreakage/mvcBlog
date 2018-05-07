0da31a1ce0ca747a977ce12eb23f77e9d60947f0
megabreakage <martin.njau@live.com>
2018-05-07 16:40:24 +0300

    Creates a Dashboard to allow naviagtion and creates a link to add posts

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,18 +1,36 @@
 
+<?php
+if (isset($_SESSION['user_loggedin'])) { ?>
+
+  <style media="screen">
+    li{
+      list-style-type: none;
+    }
+  </style>
 
  <div class="container">
+   <h1 class="text-center animated bounceInDown pt">Dashboard</h1>
 
-
-   <h2 class="text-center FadeIn">Dashboard</h2>
-
-   <?php
-      echo "<pre>";
-          print_r($_SESSION['username']);
-      echo "<pre>";
-
-      echo form_open('blogs/logout');
-      echo form_submit(['name'=>'submit', 'value'=>'SIGN OUT', 'class'=>'btn buttons']);
-      echo form_close();
-
-    ?>
+   <div class="row">
+     <div class="col-md-3">
+       <h5 class="">Welcome <a href=""> <strong class="animated bounceIn"><?php echo $_SESSION['username']; ?></strong></a>!</h5>
+       <hr>
+       <li class="text-center">
+         <?php echo anchor("dashboard/add_post", 'ADD BLOG POST') ?>
+       </li>
+       <hr>
+     </div>
+     <div class="col-md-9 columns pt">
+       <h4 class="text-center animated bounceIn">READ OUR BLOGS</h4>
+       
+     </div>
+   </div>
  </div>
+
+ <?php
+}
+else{
+  return redirect('blogs', 'refresh');
+}
+
+ ?>
