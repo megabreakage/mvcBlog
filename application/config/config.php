8527423d2f24f9b21d095b60dfb4dd8624b2cb6f
megabreakage <martin.njau@live.com>
2018-05-07 14:39:27 +0300

    Sets compress_output = TRUE and sets an Envryption Key

--- application/config/config.php
+++ application/config/config.php
@@ -325,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '123456789s';
 
 /*
 |--------------------------------------------------------------------------
@@ -476,7 +476,7 @@ $config['csrf_exclude_uris'] = array();
 | by the output class.  Do not 'echo' any values with compression enabled.
 |
 */
-$config['compress_output'] = FALSE;
+$config['compress_output'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
