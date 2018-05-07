f62eb63a56a3223a911cd6563126264cb3fa8bf9
megabreakage <martin.njau@live.com>
2018-05-07 14:40:48 +0300

    Adds dashboard and it's methods as routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['blogs'] = 'blogs';
-$route['default_controller'] = 'welcome';
+$route['dashboard/add_post'] = 'dashboard/add_post';
+$route['dashboard'] = 'dashboard';
+$route['welcome'] = 'welcome';
+$route['default_controller'] = 'blogs';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
