c3955b135874659a1db6868a4474356b5c17ee3c
megabreakage <martin.njau@live.com>
2018-05-09 16:07:48 +0300

    Creates methods to get single and multiple posts

--- application/models/Queries.php
+++ application/models/Queries.php
@@ -19,4 +19,14 @@ class Queries extends CI_Model{
     $query = $this->db->insert('tbl_posts', $data);
   }
 
+  public function get_posts(){
+    $query = $this->db->get('tbl_posts');
+    return $query->result_array();
+  }
+
+  public function get_single_post($post_id){
+    $query =  $this->db->get_where('tbl_posts', array('post_id'=>$post_id));
+    return $query->row_array(); //returns results as a row and not stdClass object
+  }
+
 } ?>
